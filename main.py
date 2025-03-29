78995df27e3b55148687b144232955b36491711e
Jonathanhsiao123 <yuning.hsiao@gmail.com>
2025-03-29 14:40:29 -0400

    backend function draft

---
+++ main.py
@@ -0,0 +1,24 @@
+def search_files_by_time(folder: str):
+    pass
+
+def voice_to_str(location:str):
+    pass
+
+def parse_shape_instruction(instruction: str, library:str) -> str:
+    pass
+
+def replacing_defaults(input_data):
+    pass
+
+def validate_vr_objects(json_data):
+    pass
+
+def generate_unique_key(geotype, class_name, index):
+    pass
+
+
+def save_json_string_to_file(json_string, file_path):
+    pass
+
+def main():
+    pass
